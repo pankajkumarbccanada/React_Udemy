41d78c43c4582b72fd10adf93f51a5b638e7457c
Pankaj Kumar <pk0240@gmail.com>
2026-07-30 19:11:02 -0700

    new extra text written.

--- udemy_project_react/src/Components/CardsComponent.jsx
+++ udemy_project_react/src/Components/CardsComponent.jsx
@@ -1,7 +1,9 @@
 function CardsComponent({fruits}){
     return(
     <>
-        <h1>  This is my cards component and i am uploading on static app by github work flow.</h1>
+        <h1>  This is my cards component and i am uploading on static app by github work flow.
+            yes i am doing this for the first time and i am very excited to see the result of this work flow.
+        </h1>
         <div>
             {fruits.map((frt)=>(
                 <div key={frt.key}>
